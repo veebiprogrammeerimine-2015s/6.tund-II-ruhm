99536aaf730e9767d7545c69b1784ddfa942b853
Romil Robtsenkov <romil@greeny.cs.tlu.ee>
2015-10-16 10:41:03 +0300

    edit input values

--- table.php
+++ table.php
@@ -38,11 +38,11 @@
 			if(isset($_GET["edit"]) && $car_list[$i]->id == $_GET["edit"]){
 				// kasutajale muutmiseks
 				echo "<tr>";
-					echo "<form>";
+					echo "<form action='table.php' method='post'>";
 						echo "<td>".$car_list[$i]->id."</td>";
 						echo "<td>".$car_list[$i]->user_id."</td>";
-						echo "<td><input name='number_plate'></td>";
-						echo "<td><input name='color'></td>";
+						echo "<td><input name='number_plate' value='".$car_list[$i]->number_plate."'></td>";
+						echo "<td><input name='color' value='".$car_list[$i]->color."'></td>";
 						echo "<td><input type='submit' name='update'></td>";
 						echo "<td><a href='table.php'>cancel</a></td>";
 					echo "</form>";
